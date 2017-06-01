b6a4575f924d7cb57da0488a9293b8e48271acda
HarryLottey <harry.lottey@gmail.com>
2017-06-01 15:46:28 +1000

    XML Saving

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab; // Object we want to spawn
+    public float spawnRate = 1f; // Spawn an objec tevery spawn rate interval (in seconds)
+    [HideInInspector] public List<GameObject> objects = new List<GameObject>();
+
+    public Vector3 randomPoint;
+
+    private float spawnTimer = 0f; // Counts up every frame in seconds as our timer
+
+    void OnDrawGizmos()
+    {
+        // Draw a cube to indicate where the box is that 
+        // we're spawning objects
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+    
+    // Generates a random point within the transform's scale
+    Vector3 GenerteRandomPoint()
+    {
+        // SET halfScale to half of the transform's scale
+        Vector3 halfScale = transform.localScale / 2;
+        // SET randomPoint vector to zero
+        randomPoint = Vector3.zero;
+        // SET randomPoint x,y,z to random range between
+        // -halfScale to halfScale (HINT: can do individually)
+        randomPoint.x = Random.Range(-halfScale.x, halfScale.x);
+        randomPoint.y = 0;
+        randomPoint.z = Random.Range(-halfScale.z, halfScale.z);
+        // RETURN randomPoint
+        return randomPoint;
+    }
+
+    // Spawns the prefab at a given position and with rotation
+    public void Spawn(Vector3 position, Quaternion rotation)
+    {
+        // SET clone to new instance of prefab
+        GameObject clone = Instantiate(prefab);
+        // ADD clone to objects list
+        objects.Add(clone);
+        // SET clone's posiiton to spawner position + position
+        clone.transform.position = transform.position + position;
+        // SET clone's rotation to rotation
+        clone.transform.rotation = rotation;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // SET spawnTimer to spawnTimer + delta time
+        spawnTimer = spawnTimer + Time.deltaTime;
+        // IF spawnTimer > spawnRate
+        if(spawnTimer > spawnRate)
+        {
+            // SET randomPoint to GenerateRandomPoint()
+            randomPoint = GenerteRandomPoint();
+            // CALL Spawn() and pass GenerateRandomPoint(), Quarternion identity
+            Spawn(randomPoint,Quaternion.identity);
+            // SET spawnTimer to zero
+            spawnTimer = 0f;
+        }
+
+    }
+}
